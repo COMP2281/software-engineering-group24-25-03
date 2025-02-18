a5795e65c7b44f547c785da66f309f7401a55a83
JTBellStudent <147186684+JTBellStudent@users.noreply.github.com>
2025-02-18 07:42:13 +0000

    Create HelpPage.jsx

---
+++ HelpPage.jsx
@@ -0,0 +1,135 @@
+import styles from './HelpPage.module.css';
+
+
+const HelpPage = () => {
+  	return (
+    		<div className={styles.helpPage}>
+      			<div className={styles.pageTitle}>
+        				<b className={styles.title}>Help</b>
+        				<img className={styles.heartHandshakeIcon} alt="" src="Heart Handshake.svg" />
+      			</div>
+      			<div className={styles.helpElements}>
+        				<div className={styles.videoTileParent}>
+          					<div className={styles.videoTile}>
+            						<div className={styles.videoTileChild} />
+            						<img className={styles.iconplay} alt="" src="icon/play.svg" />
+            						<div className={styles.descriptionFrame}>
+              							<div className={styles.videoDescription}>
+                								<div className={styles.howToAdd}>How to add a new task</div>
+                								<div className={styles.min}>3 min</div>
+              							</div>
+              							<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+            						</div>
+          					</div>
+          					<div className={styles.videoTile1}>
+            						<div className={styles.videoTileChild} />
+            						<img className={styles.iconplay} alt="" src="icon/play.svg" />
+            						<div className={styles.descriptionFrame}>
+              							<div className={styles.videoDescription}>
+                								<div className={styles.howToAdd}>How to add a new task</div>
+                								<div className={styles.min}>3 min</div>
+              							</div>
+              							<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+            						</div>
+          					</div>
+          					<div className={styles.videoTileGroup}>
+            						<div className={styles.videoTile2}>
+              							<div className={styles.videoTileChild} />
+              							<img className={styles.iconplay} alt="" src="icon/play.svg" />
+              							<div className={styles.descriptionFrame}>
+                								<div className={styles.videoDescription}>
+                  									<div className={styles.howToAdd}>How to add a new task</div>
+                  									<div className={styles.min}>3 min</div>
+                								</div>
+                								<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+              							</div>
+            						</div>
+            						<div className={styles.videoTile3}>
+              							<div className={styles.videoTileChild} />
+              							<img className={styles.iconplay} alt="" src="icon/play.svg" />
+              							<div className={styles.descriptionFrame}>
+                								<div className={styles.videoDescription}>
+                  									<div className={styles.howToAdd}>How to add a new task</div>
+                  									<div className={styles.min}>3 min</div>
+                								</div>
+                								<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+              							</div>
+            						</div>
+            						<div className={styles.videoTile4}>
+              							<div className={styles.videoTileChild} />
+              							<img className={styles.iconplay} alt="" src="icon/play.svg" />
+              							<div className={styles.descriptionFrame}>
+                								<div className={styles.videoDescription}>
+                  									<div className={styles.howToAdd}>How to add a new task</div>
+                  									<div className={styles.min}>3 min</div>
+                								</div>
+                								<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div className={styles.videoTileContainer}>
+          					<div className={styles.videoTile}>
+            						<div className={styles.videoTileChild} />
+            						<img className={styles.iconplay} alt="" src="icon/play.svg" />
+            						<div className={styles.descriptionFrame}>
+              							<div className={styles.videoDescription}>
+                								<div className={styles.howToAdd}>How to add a new task</div>
+                								<div className={styles.min}>3 min</div>
+              							</div>
+              							<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+            						</div>
+          					</div>
+          					<div className={styles.videoTile1}>
+            						<div className={styles.videoTileChild} />
+            						<img className={styles.iconplay} alt="" src="icon/play.svg" />
+            						<div className={styles.descriptionFrame}>
+              							<div className={styles.videoDescription}>
+                								<div className={styles.howToAdd}>How to add a new task</div>
+                								<div className={styles.min}>3 min</div>
+              							</div>
+              							<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+            						</div>
+          					</div>
+          					<div className={styles.videoTileGroup}>
+            						<div className={styles.videoTile2}>
+              							<div className={styles.videoTileChild} />
+              							<img className={styles.iconplay} alt="" src="icon/play.svg" />
+              							<div className={styles.descriptionFrame}>
+                								<div className={styles.videoDescription}>
+                  									<div className={styles.howToAdd}>How to add a new task</div>
+                  									<div className={styles.min}>3 min</div>
+                								</div>
+                								<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+              							</div>
+            						</div>
+            						<div className={styles.videoTile3}>
+              							<div className={styles.videoTileChild} />
+              							<img className={styles.iconplay} alt="" src="icon/play.svg" />
+              							<div className={styles.descriptionFrame}>
+                								<div className={styles.videoDescription}>
+                  									<div className={styles.howToAdd}>How to add a new task</div>
+                  									<div className={styles.min}>3 min</div>
+                								</div>
+                								<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+              							</div>
+            						</div>
+            						<div className={styles.videoTile4}>
+              							<div className={styles.videoTileChild} />
+              							<img className={styles.iconplay} alt="" src="icon/play.svg" />
+              							<div className={styles.descriptionFrame}>
+                								<div className={styles.videoDescription}>
+                  									<div className={styles.howToAdd}>How to add a new task</div>
+                  									<div className={styles.min}>3 min</div>
+                								</div>
+                								<div className={styles.inThisShort}>In this short video, Samantha overviews how to add a new task, showing you the useful shortcuts that will save you time and effort doing this often repeat...</div>
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div className={styles.helpElementsChild} />
+      			</div>
+    		</div>);
+};
+
+export default HelpPage;
