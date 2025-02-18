afe8585b4ca61c87073bc0a64394cc658a2fcbe7
Zack Turner <zackdavidturner@gmail.com>
2025-02-18 15:45:33 +0000

    added some comments

--- progressTrackerFrontend/src/components/authentication/Login.jsx
+++ progressTrackerFrontend/src/components/authentication/Login.jsx
@@ -4,6 +4,7 @@ import { login } from '../../redux/slices/authSlice';
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+	// don't touch this stuff when doing front end styling
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const dispatch = useDispatch()
@@ -18,10 +19,12 @@ export default function Login() {
             navigate('/'); 
         }
     }
+	// everything below this can be changed other than input boxes
   return (
     <div>
         <h5>{error_msg}</h5>
         <form onSubmit={onSubmit}>
+			{/* use these input boxes when doing the front end styling becuase they are linked to the backend */}
             <input value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='Username'/>
             <input value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Password'/>
             <button type="submit">Doner</button>
