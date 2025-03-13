d9ba53e9f48df521f7748a068f9c26759fbd0afe
Nathaniel Hughes <nathanielh2511@gmail.com>
2025-03-13 22:31:42 +0000

    Add videos to help section

--- progressTrackerFrontend/src/components/landing_page/articles/DeleteProject.jsx
+++ progressTrackerFrontend/src/components/landing_page/articles/DeleteProject.jsx
@@ -4,22 +4,16 @@ const DeleteProject = () => {
         <h2>Deleting a Project</h2>
         <p>This section explains how to delete an existing project.</p>
         <ol>
-          <li>Go to your dashboard.</li>
+          <li>Go to the admin dashboard.</li>
           <li>Find the project you want to delete.</li>
-          <li>Click on the project settings.</li>
           <li>Select &quot;Delete Project&quot;.</li>
-          <li>Type the name of the project for confirmation.</li>
           <li>The project will be permanently removed.</li>
         </ol>
         <div className="video-container">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/T3qH-uY3t-Y"
-            title="Deleting a Project Tutorial"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+        <video width="560" height="315" controls>
+          <source src="http://127.0.0.1:8000/media/videos/Delete_Task.mp4" type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
         </div>
       </article>
     );
--- progressTrackerFrontend/src/components/landing_page/articles/EditProject.jsx
+++ progressTrackerFrontend/src/components/landing_page/articles/EditProject.jsx
@@ -4,20 +4,16 @@ const EditProject = () => {
       <h2>Editing a Project</h2>
       <p>This section explains how to edit existing projects.</p>
       <ol>
-        <li>Open your dashboard</li>
+        <li>Go to the admin dashboard.</li>
         <li>Select which project you would like to edit</li>
         <li>Update the project details in the displayed form.</li>
         <li>Review the information and click &quot;Submit&quot; to update the project.</li>
       </ol>
       <div className="video-container">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/T3qH-uY3t-Y"
-          title="Creating a Project Tutorial"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+      <video width="560" height="315" controls>
+          <source src="http://127.0.0.1:8000/media/videos/Edit_Task.mp4" type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
       </div>
     </article>
   );
